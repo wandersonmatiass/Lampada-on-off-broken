6ed83d9e305efa63832e1258d5986b468a2d9655
Wanderson Matias <102179463+wandersonmatiass@users.noreply.github.com>
2022-03-28 19:33:47 +0100

    Alterações

---
+++ script.js
@@ -0,0 +1,10 @@
+const turnOn = document.getElementById('turnOn')
+const turnOff = document.getElementById('turnOff')
+const lamp = document.getElementById('lamp') 
+
+function lampOn () {
+    lamp.src = "./img/ligada.jpg"
+}
+
+
+turnOn.addEventListener ('click' , lampOn ) 
