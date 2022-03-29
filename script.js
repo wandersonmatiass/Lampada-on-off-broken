9648050ed88bd7c785d5e6ed0a9331c4dd5a64be
Wanderson Matias <102179463+wandersonmatiass@users.noreply.github.com>
2022-03-29 20:12:18 +0100

    Correção

--- script.js
+++ script.js
@@ -2,21 +2,31 @@ const turnOn = document.getElementById ( 'turnOn' );
 const turnOff = document.getElementById ( 'turnOff' );
 const lamp = document.getElementById ( 'lamp' );
 
+function isLampBroken () {
+
+    return lamp.src.indexOf ( 'quebrada' ) > -1
+}
 
 function lampOn () {
 
-   lamp.src = './img/ligada.jpg'
+    if ( !isLampBroken ()) {
+        lamp.src = './img/ligada.jpg'
+    }
+
 }
 
 function lampOff() {
 
+    if ( !isLampBroken ()) {
     lamp.src = './img/desligada.jpg'
+    }
 }
 
 function lampBroken() {
 
     lamp.src = './img/quebrada.jpg'
 }
+
 
 turnOn.addEventListener ( 'click', lampOn );
 turnOff.addEventListener ( 'click', lampOff );
